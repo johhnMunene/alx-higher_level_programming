9c3dde670d126e2bce17c93e2236047852becf86
johhnmunene <edwardsjohnny64@gmail.com>
2023-08-10 18:04:09 +0300

    6-print_comb3.py error

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 for m in range(9):
-    for n in range(i + 1, 10):
+    for n in range(n + 1, 10):
         print("{:d}{:d}".format(m, n), end="")
         if i < 8:
             print(", ", end="")
