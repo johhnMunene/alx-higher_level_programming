4fe86bddb7bc87942f97da36df26cbcf4702a6d3
johhnmunene <edwardsjohnny64@gmail.com>
2023-08-10 23:07:47 +0300

    8-uppercase.py error

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+
 def uppercase(s):
+    result = ""
     for c in s:
-        if 'a' <= c <= 'z':
-            print("{:c}".format(ord(c) - 32), end="")
-        else:
-            print(c, end="")
+        if ord(c) >= 97 and ord(c) <= 122:
+            c = chr(ord(c) - 32)
+        result += c
+    print(result)
+
