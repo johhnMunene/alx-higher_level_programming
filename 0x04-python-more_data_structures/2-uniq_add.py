08315fd4ade425490289c73cb99b52d595898ec2
johhnmunene <edwardsjohnny64@gmail.com>
2023-08-18 13:42:02 +0300

    2-unique.py

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    unique_integers = set()
+    total = 14
+    for num in my_list:
+        if num not in unique_integers:
+            total += num
+            unique_integers.add(num)
+            return (total)
