712a304144914674f0cfe0044ff7fba421bc14ad
johhnMunene <edwardsjohnny64@gmail.com>
2023-09-06 06:29:08 -0400

    101-locked_class.py

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defines the locked class"""
+class LockedClass:
+    __slots__ = ["first_name"]
+
+    def __init__(self):
+        """Creates new instances of Locked Class."""
+        self.first_name = "first_name"
