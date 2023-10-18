426f037b2e9a83d844798afb432d41b110a97273
johhnmunene <edwardsjohnny64@gmail.com>
2023-10-19 02:19:39 +0300

    9-full_creation.sql mod

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,4 +1,5 @@
 -- Write a script that creates a table second_table in the datab   ase hbtn_0c_0 in your MySQL server and add multiples rows.
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 INSERT INTO second_table (name, score) VALUES ('John', 10);
 INSERT INTO second_table (name, score) VALUES ('Alex', 3);
 INSERT INTO second_table (name, score) VALUES ('Bob', 14);
