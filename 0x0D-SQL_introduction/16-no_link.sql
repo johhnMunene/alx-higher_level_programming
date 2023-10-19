30c473a3f5c40e89082a9bd0b20a15500ecd8409
johhnmunene <edwardsjohnny64@gmail.com>
2023-10-19 15:51:18 +0300

    list all records from the "second_table" of the "hbtn_0c_0"mod

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,6 +1,6 @@
--- script that lists all records of the table second_table of the database hbtn_0c_0
+-- lists records of database
 SELECT score, name
-FROM hbtn_0c_0.second_table
+FROM second_table
 WHERE name IS NOT NULL
 ORDER BY score DESC;
 
