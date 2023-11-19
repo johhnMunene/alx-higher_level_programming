ba5f80f5850f8c75c8aa21601e1675c5e0467014
johhnmunene <edwardsjohnny64@gmail.com>
2023-11-20 01:34:50 +0300

    listing all states in my database correction

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,23 +1,17 @@
 #!/usr/bin/python3
-""" Script that lists all states from the database hbtn_0e_0_usa """
+"""
+Lists all states from the database hbtn_0e_0_usa
+"""
+import sys
 import MySQLdb
-from sys import argv
 
-# The code should not be executed when imported
 if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
 
-    # make a connection to the database
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3])
-
-    # It gives us the ability to have multiple seperate working environments
-    # through the same connection to the database.
     cur = db.cursor()
-    cur.execute("SELECT * FROM states")
+    cur.execute("SELECT * FROM states;")
+    states = cur.fetchall()
 
-    rows = cur.fetchall()
-    for i in rows:
-        print(i)
-    # Clean up process
-    cur.close()
-    db.close()
+    for state in states:
+        print(state)
