8861cc8eccdaa49ed08f3f4b3002f423e62a649f
johhnmunene <edwardsjohnny64@gmail.com>
2023-11-20 02:08:12 +0300

    0-select_states. correction

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+"""
+a script that takes in an argument and displays
+all values in the states table of hbtn_0e_0_usa
+where name matches the argument.
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    "command line arguments"
+    user = argv[1]
+    passwd = argv[2]
+    db = argv[3]
+    state = argv[4]
+
+    "connection to my database"
+    db = MySQLdb.connect(port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+    cursor = db.cursor()
+
+    query = ("SELECT * FROM states "
+             "WHERE name LIKE BINARY '{}' "
+             "ORDER BY states.id ASC".format(state))
+    cursor.execute(query)
+
+    "fetch all the rows"
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    "close the cursor and connection"
+    cursor.close()
+    db.close()
