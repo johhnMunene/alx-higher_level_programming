7dd40c5d713febf2331111405f7c32dd417b07b8
johhnmunene <edwardsjohnny64@.com>
2023-11-30 19:38:10 -0600

    3-methods.sh correction

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Use curl to send a HEAD request (-I option) to the specified URL (-X option).
-curl -sI -X "$1" | grep Allow | cut -d ' ' -f2-
+# Displays all HTTP methods that the server will accept
+curl -sI "$1" | grep Allow | cut -d ' ' -f2-
