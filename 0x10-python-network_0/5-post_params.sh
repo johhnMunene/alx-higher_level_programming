14654d05b741579b3bc529785b98f1e431697226
johhnmunene <edwardsjohnny64@.com>
2023-11-30 19:56:45 -0600

    5-post_params.sh POST correction

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#5-post_params.sh
-curl -sL -X '$1" POST  -d  "email=test@gmail.com&subject=I will always be here for PLD"
+# post parameters
+curl -sL "$1" -X POST -d "email=test@gmail.com&subject=I will always be here for PLD"
