146c9b2c795cb6b3c9572e24e26958bfa599c7a8
johhnmunene <edwardsjohnny64@.com>
2023-11-30 18:42:48 -0600

    0-body_size.sh requirement

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
- #!/bin/bash
- #Bash script that takes in a URL, sends a request to that URL 
- curl -sI "$1" | grep -i Content-Length | cut -d ' ' -f 2
+#!/bin/bash
+# cURL get body size
+curl -sI "$1" | grep -i Content-Length | cut -d ' ' -f 2
