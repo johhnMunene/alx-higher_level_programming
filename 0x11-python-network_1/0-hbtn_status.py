50648fe106ba5ef4992738de12d24485c8d8121b
johhnmunene <edwardsjohnny64@.com>
2023-12-05 08:23:44 -0600

    updates

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-# Python script that fetches https://alx-intranet.hbtn.io/status
-
+# fetching a page using urllib
 import urllib.request
 
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+quest = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+with urllib.request.urlopen(quest) as response:
     html = response.read()
-    print("Body response:")
-    print("\t- type:", type(html))
-    print("\t- content:", html.decode('utf-8'))
+
+print('Body response:')
+print('\t- type:', type(html))
+print('\t- content:', html)
+print('\t- utf8 content:', html.decode('utf-8'))
