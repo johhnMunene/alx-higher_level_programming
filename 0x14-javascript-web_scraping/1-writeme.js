7d5af816faf74f3ed1122a360e667c6125b16c49
johhnMunene <Edwardsjohnny64@gmail.com>
2024-01-30 20:35:46 -0600

    1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,18 +1,6 @@
 #!/usr/bin/node
 // script that writes a string to a file.:
-
 const fs = require('fs');
-
-const writeToFile = (filePath, content) => {
-  try {
-    fs.writeFile(filePath, content, 'utf-8', (err) => {
-      if (err) {
-        console.error(err);
-        return;
-      }
-      console.log(`Content written to ${filePath}`);
-    });
-  } catch (error) {
-    console.error(error);
-  }
-};
+fs.writeFile(process.argv[2], process.argv[3], error => {
+  if (error) console.log(error);
+});
